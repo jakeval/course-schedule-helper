10d630553c68de7b778a2b10464c795c0d5e9d31
jason <jasonvallada@umass.edu>
2018-11-03 00:11:09 -0400

    added superuser, adjusted settings.py

--- schedulepermuter/main/admin.py
+++ schedulepermuter/main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Institution, Department, Course, Professor, Section
 
-# Register your models here.
+admin.site.register(Institution)
+admin.site.register(Department)
+admin.site.register(Course)
+admin.site.register(Professor)
+admin.site.register(Section)
